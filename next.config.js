b45323463f735ab8176c799ea2364e42be48d581
Not TCA <realtcatech@gmail.com>
2022-12-21 09:45:59 +0800

    redirect on /invite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/invite",
+        destination:
+          "https://discord.com/oauth2/authorize?client_id=1054359549219459133&permissions=1099914307614&scope=bot%20applications.commands",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
