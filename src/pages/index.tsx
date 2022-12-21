b94da8ee29254f550569d4c9f2943affe72d35c7
Not TCA <realtcatech@gmail.com>
2022-12-21 09:41:27 +0800

    make invite button actually work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,12 +14,11 @@ export default function Home() {
             commands that are easy to understand.
           </p>
           <div className="p-2 md:p-3"></div>
-          <button
-            className="px-3 py-2 rounded-md bg-orange-600 text-white opacity-50 cursor-not-allowed"
-            title="Coming soon!"
-          >
-            Invite to your server
-          </button>
+          <Link href="https://discord.com/oauth2/authorize?client_id=1054359549219459133&permissions=1099914307614&scope=bot%20applications.commands">
+            <button className="px-3 py-2 rounded-md bg-orange-600 hover:bg-orange-700 text-white transition-colors duration-150">
+              Invite to your server
+            </button>
+          </Link>
         </div>
         <Image
           src="/logo.png"
