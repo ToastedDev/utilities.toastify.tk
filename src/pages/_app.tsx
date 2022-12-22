f05c5d8e06b25e773dc29c1f80218d93acb410c6
Not TCA <realtcatech@gmail.com>
2022-12-22 15:03:57 +0800

    cooler font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { seo } from "../utils/seo";
 
-import { Inter } from "@next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import { Rubik } from "@next/font/google";
+const rubik = Rubik({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <DefaultSeo {...seo} />
       <style jsx global>{`
         html {
-          font-family: ${inter.style.fontFamily}, "sans-serif";
+          font-family: ${rubik.style.fontFamily}, "sans-serif";
         }
       `}</style>
       <Component {...pageProps} />
